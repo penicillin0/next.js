271dcf33a07389c9c0c7b39766060efb37859592
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-24 11:25:22 +0200

    WIP: logging

--- turbopack/crates/turbo-tasks-backend/src/backend/operation/aggregation_update.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/operation/aggregation_update.rs
@@ -142,6 +142,7 @@ impl AggregatedDataUpdate {
         let Self {
             dirty_container_update,
         } = self;
+        println!("apply {:?} {dirty_container_update:?}", task.id());
         let mut result = Self::default();
         if let Some((dirty_container_id, count)) = dirty_container_update {
             let mut added = false;
@@ -187,11 +188,19 @@ impl AggregatedDataUpdate {
                 }
                 (new != 0).then_some(new)
             });
+            println!(
+                "apply {task_id:?} {dirty_container_update:?} added={added:?} removed={removed:?} \
+                 count_update={count_update} => {:?}",
+                result.dirty_container_update
+            );
             if let Some((_, count)) = result.dirty_container_update.as_ref() {
                 if let Some(root_state) = get!(task, AggregateRoot) {
                     if *count < 0 {
                         root_state.all_clean_event.notify(usize::MAX);
                         if matches!(root_state.ty, ActiveType::CachedActiveUntilClean) {
+                            println!(
+                                "apply {task_id:?} {dirty_container_update:?} removed RootState"
+                            );
                             task.remove(&CachedDataItemKey::AggregateRoot {});
                         }
                     }
@@ -251,6 +260,7 @@ impl AggregationUpdateQueue {
     }
 
     pub fn push(&mut self, job: AggregationUpdateJob) {
+        println!("push {:?}", job);
         self.jobs.push_back(job);
     }
 
@@ -267,6 +277,7 @@ impl AggregationUpdateQueue {
     pub fn process(&mut self, ctx: &ExecuteContext<'_>) -> bool {
         self.stats.processed_jobs += 1;
         if let Some(job) = self.jobs.pop_front() {
+            println!("process {:?}", job);
             match job {
                 AggregationUpdateJob::UpdateAggregationNumber {
                     task_id,
@@ -285,6 +296,15 @@ impl AggregationUpdateQueue {
                     let aggregation_number = base_aggregation_number.saturating_add(distance);
                     if old >= aggregation_number {
                         if base_aggregation_number != current.base && distance != current.distance {
+                            println!(
+                                "UpdateAggregationNumber meta {:?} base {} -> {}, distance {} -> \
+                                 {}",
+                                task_id,
+                                current.base,
+                                base_aggregation_number,
+                                current.distance,
+                                distance
+                            );
                             task.insert(CachedDataItem::AggregationNumber {
                                 value: AggregationNumber {
                                     base: base_aggregation_number,
@@ -294,6 +314,17 @@ impl AggregationUpdateQueue {
                             });
                         }
                     } else {
+                        println!(
+                            "UpdateAggregationNumber {:?} {} -> {}, base {} -> {}, distance {} -> \
+                             {}",
+                            task_id,
+                            old,
+                            aggregation_number,
+                            current.base,
+                            base_aggregation_number,
+                            current.distance,
+                            distance
+                        );
                         task.insert(CachedDataItem::AggregationNumber {
                             value: AggregationNumber {
                                 base: base_aggregation_number,
@@ -573,6 +604,7 @@ impl AggregationUpdateQueue {
                         if task.has_key(&CachedDataItemKey::Dirty {}) {
                             let description = ctx.backend.get_task_desc_fn(task_id);
                             if task.add(CachedDataItem::new_scheduled(description)) {
+                                println!("Scheduled {:?}", task_id);
                                 ctx.turbo_tasks.schedule(task_id);
                             }
                         }
@@ -597,6 +629,11 @@ impl AggregationUpdateQueue {
                         ctx.task_pair(upper_id, task_id, TaskDataCategory::Meta);
                     let upper_aggregation_number = get_aggregation_number(&upper);
                     let task_aggregation_number = get_aggregation_number(&task);
+                    println!(
+                        "BalanceEdge {:?} {upper_aggregation_number} {:?} \
+                         {task_aggregation_number}",
+                        upper_id, task_id
+                    );
 
                     let should_be_inner = is_root_node(upper_aggregation_number)
                         || upper_aggregation_number > task_aggregation_number;
@@ -695,7 +732,12 @@ impl AggregationUpdateQueue {
             }
         }
 
-        self.jobs.is_empty()
+        if self.jobs.is_empty() {
+            println!("done");
+            true
+        } else {
+            false
+        }
     }
 }
 
